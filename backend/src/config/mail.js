56eb162a0a201e6ac17b688b0b6f572f138aaf18
Jessica Castro <jessica.castros@outlook.com>
2020-04-15 13:05:05 -0300

    Backend with nodemailer

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '24b3d753d1d97d',
+    pass: 'bdfabf7afb829b',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,53 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'API works!' });
 });
+
+/**
+ * SESSIONS
+ */
+routes.post('/sessions', SessionController.store);
+
+/**
+ * USERS
+ */
+routes.post('/users', UserController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
+
+/**
+ * APPOINTMENTS
+ */
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
+routes.get('/schedule', ScheduleController.index);
+
+/**
+ * NOTIFICATIONS
+ */
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 export default routes;
